9b80c293658f5cf7e5d8a01648c1d6b33cb2e255
Jlouisa <jonathan.louisa@gmail.com>
2023-09-08 14:05:21 +0200

    Merging two javascript files

--- javascript.js
+++ javascript.js
@@ -19,28 +19,6 @@
 // }
 // theKnight();
 
-//! Create Class
-class Pieces {
-  constructor(name) {
-    this.name = name;
-    this.position = position;
-  }
-}
-
-class chessBoard {
-  constructor(arrXY) {
-    this.root = "Head";
-  }
-  createBoard(arrX, arrY) {
-    arrX.array.forEach((elementX) => {
-      arrY.forEach((elementY) => {
-        let node = [elementX, elementY];
-        this.root = node;
-      });
-    });
-  }
-}
-
 // const gameBoardDown = (x, y) => {
 //   if (x < 0 || x > 7) return;
 //   return gameBoard(x - 1, y - 2) + gameBoard(x - 2, y - 1);
@@ -56,16 +34,6 @@ class chessBoard {
 
 //Create 1 tree from arrX and arrY
 //Create balanced tree from arrXY
-
-const positionX = [0, 1, 2, 3, 4, 5, 6, 7];
-const positionY = [0, 1, 2, 3, 4, 5, 6, 7];
-
-let theArray = [];
-for (let x of positionX) {
-  for (let y of positionY) {
-    theArray.push([x, y]);
-  }
-}
 
 /* theArray = 
 [
@@ -84,28 +52,114 @@ for (let x of positionX) {
   [ 7, 4 ], [ 7, 5 ], [ 7, 6 ], [ 7, 7 ]
 ]
 */
+/*
+Question:
+Hey i'm asking about the knight travails project, 
+do we need to implement a graph? Because the lesson 
+just gave a single article on the data structure and 
+i don't think I can implement it with that little info, 
+or should I just do my own research on graphs?
 
-class Pieces {
-  constructor(name) {
-    this.pieceName = name;
-    this.position = null;
-  }
+Answer:
+There's more than one way to represent a graph, 
+you don't need your graph to be real `nodes` and `vertices`, 
+you just need a way to treat your data in such a way that it's `nodes` and `vertices`.
+For example a 2X2 array can represent the following graph:
+```js
+0---0
+| X |
+0---0
+```
+a graph where every square is connected, 
+for example [0][0] is a node that's connected to those nodes (and vice versa):
+```js
+[0][0] -> [0][1]
+ |     \
+ V      V
+[1][0]   [1][1]
+```
+
+In the end is all about how **you** work with your data.
+*/
+
+// Starting point and Destination Input
+// All stops to and including Starting point & Destination Output
+// Every node === a position on the board
+// Every child node === a valid move for the Knight
+// Valid moves === 1 + 2 <= 7,7 || 2 + 1 <= 7,7 || 2 - 1 >= 0,0 || 1 - 2 >= 0,0
+// Root of tree is starting point of Knight
+// Legal moves: 0.0 <= 1.2 + 2.1 + 0.8 + 1.9 >= 7.7
+// 6 branches, but need to avoid infinite
+
+/*
+x = +2x / +1y
+y = +1x / +2y
+
+Node = {
+    coor: 1.2,
+    savedPiece: none,
+    nextPosX1: 1.2,  (+2, +1)
+    nextPosX2: null, (+2, -1)
+    nextPosY1: 2.1,  (+1, +2)
+    nextPosY2: null, (-1, +2)
+    nextNegY1: null, (+1, -2)
+    nextNegX1: null, (-2, +1)
+    nextNegX2: null, (-2, -1)
+    nextNegY2: null, (-1, -2)
+    previous: 0.0,
 }
+*/
+
+//! Compare coordinates
+const compareCoor = (coor1, coor2) => {
+  return coor1[0] === coor2[0] && coor1[1] === coor2[1];
+};
+
+//! Create gameBoard Coordinate
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(coord) {
+    this.coord = coord;
+    this.savedPiece = null;
     this.leftNode = null;
     this.rightNode = null;
-    this.piece = null;
-  }
-}
-class GameBoard {
-  constructor(_arr, _beginIndex, _lastIndex) {
-    this.root = buildTree(_arr, _beginIndex, _lastIndex);
-    this.piece = null;
+    this.prevNode = null;
+    this.nextMoves = {
+      nextPosX1: calcNextCoord(coord, posX1),
+      nextPosX2: calcNextCoord(coord, posX2),
+      nextPosY1: calcNextCoord(coord, posY1),
+      nextPosY2: calcNextCoord(coord, posY2),
+      nextNegX1: calcNextCoord(coord, negX1),
+      nextNegX2: calcNextCoord(coord, negX2),
+      nextNegY1: calcNextCoord(coord, negY1),
+      nextNegY2: calcNextCoord(coord, negY2),
+    };
   }
 }
 
+//! Create gameBoard
+class GameBoard {
+  constructor(arr) {
+    this.root = "Head";
+    this.leftNode = null;
+    this.rightNode = null;
+    this.prevNode = null;
+    this.coor = this.createChessBlocks(arr);
+  }
+}
+
+//! Create coordinate Array
+const createGameCoord = (arrXY) => {
+  let newArr = [];
+  arrXY.forEach((coordX) => {
+    arrXY.forEach((coordY) => {
+      let nodeXY = [coordX, coordY];
+      newArr.push(nodeXY);
+    });
+  });
+  return newArr;
+};
+
+//! Build BST
 function buildTree(arr, beginIndex, lastIndex) {
   if (beginIndex > lastIndex) {
     return null;
@@ -118,8 +172,33 @@ function buildTree(arr, beginIndex, lastIndex) {
   }
 }
 
-const knight = new Pieces("Knight");
-const chessBoard = new GameBoard(theArray, 0, theArray.length - 1);
+//! Gameboard Coordinates
+const coorXY = [0, 1, 2, 3, 4, 5, 6, 7];
+
+//! Delta neighbour Coordinates
+const posX1 = [2, 1];
+const posX2 = [2, -1];
+const posY1 = [1, 2];
+const posY2 = [-1, 2];
+const negX1 = [-2, 1];
+const negY1 = [1, -2];
+const negX2 = [-2, -1];
+const negY2 = [-1, -2];
+
+//! Calculate Next Coordinate
+function calcNextCoord(pos, deltaXY) {
+  const newPos = [pos[0] + deltaXY[0], pos[1] + deltaXY[1]];
+  return newPos[0] < 0 || newPos[0] > 7 || newPos[1] < 0 || newPos[1] > 7 ? null : newPos;
+}
+
+const boardCoords = [...createGameCoord(coorXY)];
+// const linkedBoard = buildTree(boardCoords, 0, boardCoords.length - 1);
+// console.log(linkedBoard);
+
+const headNode = new Node(boardCoords[27]);
+console.log(headNode);
+
+// console.log((test = new Node([2, 1])));
 
 //! Visualize the tree in the console
 const prettyPrint = (node, prefix = "", isLeft = true) => {
