4a1d548d2bb42c2e4a617b3187a6a35481a3673a
Jlouisa <jonathan.louisa@gmail.com>
2023-09-09 21:02:28 +0200

    Make more readable in console

--- javascript.js
+++ javascript.js
@@ -262,7 +262,7 @@ const knightMoves = (root, dist) => {
   }
   // Return the shortArr after the loop finishes
   console.log(`=> You made it in ${shortArr.length} moves moves! Here's your path: `);
-  return shortArr.reverse();
+  return shortArr.reverse().forEach((n) => console.log(n));
 };
 
 const toBegin = (() => {
